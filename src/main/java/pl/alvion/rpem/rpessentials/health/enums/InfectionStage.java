18e1c2e1a712e36943410d2f2165e621151092a1
DomiRusz24 <46862541+DomiRusz24@users.noreply.github.com>
2020-04-11 23:34:44 +0200

    Stage infekcji (1, 2, 3, 4)

---
+++ src/main/java/pl/alvion/rpem/rpessentials/health/enums/InfectionStage.java
@@ -0,0 +1,25 @@
+package pl.alvion.rpem.rpessentials.health.enums;
+
+public enum InfectionStage {
+    Low("Poczatek", 1),
+    Medium("Wstepne objawy", 2),
+    High("Wszystkie objawy", 3),
+    Highest("Smiertelne", 4);
+
+
+    private String PolishId;
+    private int id;
+
+    InfectionStage(String PolishID, int id) {
+        this.id = id;
+        this.PolishId = PolishID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPolishId() {
+        return PolishId;
+    }
+}
